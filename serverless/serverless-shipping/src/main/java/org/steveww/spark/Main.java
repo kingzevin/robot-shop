6e46e7ae0cc5746467ef2ef24fd4c60c20893945
King.Zevin <king.zevin@gmail.com>
2020-08-03 13:05:20 +0800

    [done] shipping

--- serverless/serverless-shipping/src/main/java/org/steveww/spark/Main.java
+++ serverless/serverless-shipping/src/main/java/org/steveww/spark/Main.java
@@ -13,11 +13,40 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+import org.apache.commons.codec.Charsets;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.apache.commons.dbutils.DbUtils;
+import org.apache.commons.codec.binary.Base64;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+// import Base64.*;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.security.cert.CertificateException;  
+import java.security.cert.X509Certificate;  
+import java.security.SecureRandom;  
+import javax.net.ssl.SSLContext;  
+import javax.net.ssl.TrustManager;  
+import javax.net.ssl.X509TrustManager;  
+import org.apache.http.conn.ClientConnectionManager;  
+import org.apache.http.conn.scheme.Scheme;  
+import org.apache.http.conn.scheme.SchemeRegistry;  
+import org.apache.http.conn.ssl.SSLSocketFactory;  
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -29,27 +58,105 @@ import java.sql.Types;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
+// import java.util.Base64;
 
+// todo: 
+//      ssl
+//      body base64
 public class Main {
+    // zevin
+    private static int port = 8081;
+
     private static String CART_URL = null;
     private static String JDBC_URL = null;
     private static Logger logger = LoggerFactory.getLogger(Main.class);
     private static ComboPooledDataSource cpds = null;
 
-    public static JsonObject main(JsonObject args) {
-        System.out.println("2");
-        String name = "stranger";
-        if (args.has("name"))
-            name = args.getAsJsonPrimitive("name").getAsString();
-        JsonObject response = new JsonObject();
-        response.addProperty("greeting", "Hello " + name + "!");
-        return response;
+    public static JsonObject main(JsonObject args) throws Exception{
+        String[] s = {""};
+        Main.main(s);
+
+        JsonObject result = new JsonObject();
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpResponse response;
+        // URI uri = new URIBuilder();
+        if(args.has("__ow_method")){
+            String url = "http://localhost:" + port + args.get("__ow_path").getAsString();
+            if(args.get("__ow_query").getAsString().length() > 0){
+                url += "?" + args.get("__ow_query").getAsString();
+            }
+            String headers = new Gson().toJson(args.getAsJsonObject("__ow_headers"));
+            String body = args.get("__ow_body").getAsString();
+            if(body.length() > 0){
+                // body exists
+                if(Base64.isBase64(body.getBytes())){
+                    body = new String(Base64.decodeBase64(body));
+                }
+            }
+            switch (args.get("__ow_method").getAsString()){
+                case "get":
+                {
+                    HttpGet request = new HttpGet(url);
+                    setHeaders(request, headers);
+                    response = httpclient.execute(request);
+                    break;
+                }
+                case "post":
+                {
+                    HttpPost request = new HttpPost(url);
+                    setHeaders(request, headers);
+                    request.setEntity(new StringEntity(body));
+                    response = httpclient.execute(request);
+                    System.out.println(response);
+                    break;
+                }
+                default:
+                    result.addProperty("body", "No method is found!");
+                    return result;
+            }
+            setHeaders(result, response);
+            result.addProperty("body", EntityUtils.toString(response.getEntity(), Charsets.UTF_8));
+            return result;
+        }
+        else{
+            result.addProperty("body", "No method is found!");
+            return result;
+        }
+    }
+
+    private static void setHeaders(HttpRequestBase request, String headers){
+        // Dependency bug with: JsonObject.keySet() 
+        //      compile passed while no method found when running.
+        try {
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(headers);
+            for(Object key : jsonObject.keySet()){
+                request.setHeader(String.valueOf(key), String.valueOf(jsonObject.get(key)));
+            }
+        } catch (ParseException e) {
+            System.out.println("ParseError!");
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void setHeaders(JsonObject result, CloseableHttpResponse response){
+        JsonObject headers = new JsonObject();
+
+        for(Header h : response.getAllHeaders()){
+            headers.addProperty(h.getName(), h.getValue());
+        }
+
+        result.add("headers", headers);
     }
 
     public static void main(String[] args) {
         // Get ENV configuration values
-        CART_URL = String.format("http://%s/shipping/", System.getenv("CART_ENDPOINT") != null ? System.getenv("CART_ENDPOINT") : "cart");
-        JDBC_URL = String.format("jdbc:mysql://%s/cities?useSSL=false&autoReconnect=true", System.getenv("DB_HOST") != null ? System.getenv("DB_HOST") : "mysql");
+        // Get ENV configuration values
+        // CART_URL = String.format("http://%s/shipping/", System.getenv("CART_ENDPOINT") != null ? System.getenv("CART_ENDPOINT") : "cart");
+        CART_URL = "https://172.17.0.1/api/v1/web/guest/robotshop/cart/shipping/";
+        // JDBC_URL = String.format("jdbc:mysql://%s/cities?useSSL=false&autoReconnect=true", System.getenv("DB_HOST") != null ? System.getenv("DB_HOST") : "mysql");
+        JDBC_URL = "jdbc:mysql://172.18.0.1:3306/cities?useSSL=false&autoReconnect=true";
 
         //
         // Create database connector
@@ -72,7 +179,7 @@ public class Main {
         }
 
         // Spark
-        Spark.port(8080);
+        Spark.port(port);
 
         Spark.get("/health", (req, res) -> "OK");
 
@@ -235,10 +342,33 @@ public class Main {
             HttpConnectionParams.setConnectionTimeout(httpParams, 5000);
 
             httpClient = new DefaultHttpClient(httpParams);
+
+            // zevin
+            SSLContext ssl_ctx = SSLContext.getInstance("TLS");
+            TrustManager[] certs = new TrustManager[] { new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+         
+                public void checkClientTrusted(X509Certificate[] certs, String t) {
+                }
+         
+                public void checkServerTrusted(X509Certificate[] certs, String t) {
+                }
+            } };
+            ssl_ctx.init(null, certs, new SecureRandom());
+            SSLSocketFactory ssf = new SSLSocketFactory(ssl_ctx, SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+            ClientConnectionManager ccm = httpClient.getConnectionManager();
+            SchemeRegistry sr = ccm.getSchemeRegistry();
+            sr.register(new Scheme("https", 443, ssf));
+
+            httpClient = new DefaultHttpClient(ccm, httpClient.getParams());
+
             HttpPost postRequest = new HttpPost(CART_URL + id);
             StringEntity payload = new StringEntity(data);
             payload.setContentType("application/json");
             postRequest.setEntity(payload);
+
             HttpResponse res = httpClient.execute(postRequest);
 
             if(res.getStatusLine().getStatusCode() == 200) {
